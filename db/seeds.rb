dd1fdc9a7fac994f5296a03a92a13d129684bf1e
alemba-21 <alembakiragu@gmail.com>
2022-09-30 09:13:12 +0300

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Student.create({first_name: 'Alex', last_name: 'Alemba', grade: 80})
+Student.create({first_name: 'lex', last_name: 'lemba', grade: 50})
+Student.create({first_name: 'Ale', last_name: 'emba', grade: 30})
